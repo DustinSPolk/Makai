3bdd3b575d5f2020803338c1d4a7b2f501df1fb5
DustinSPolk <122975992+DustinSPolk@users.noreply.github.com>
2023-05-10 11:48:40 -0400

    Update IMessageService.cs

--- Chat/.NET/IMessageService.cs
+++ Chat/.NET/IMessageService.cs
@@ -1,14 +1,24 @@
     public interface IMessageService
     {
         int Add(MessageAddRequest model, int userId);
+        
         void Delete(int id);
+        
         Paged<MessageDetails> GetAll(int pageIndex, int pageSize);
+        
         Paged<MessageDetails> GetByCreatedBy(int senderId, int pageIndex, int pageSize);
+        
         MessageDetails GetById(int id);
+        
         Paged<MessageDetails> GetByRece(int recipientId, int pageIndex, int pageSize);
+        
         MessageDetails MapSingleMessage(IDataReader reader, ref int startingIndex);
+        
         void Update(MessageUpdateRequest model, int userId);
+        
         Task<string> UserConnected(int userId, string connectionId);
+        
         List<MessageDetails> GetByUsers(int firstUserId, int secondUserId);
+        
         List<UniqueChatContact> GetUniqueByUserId(int userId);
     }
