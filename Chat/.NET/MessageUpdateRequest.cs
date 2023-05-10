156f498a63d1144e57447eb19de5c48acca0a710
DustinSPolk <122975992+DustinSPolk@users.noreply.github.com>
2023-05-10 11:49:39 -0400

    Update MessageUpdateRequest.cs

--- Chat/.NET/MessageUpdateRequest.cs
+++ Chat/.NET/MessageUpdateRequest.cs
@@ -1,14 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Requests.Messages
-{
     public class MessageUpdateRequest : MessageAddRequest , IModelIdentifier
     {
         public int Id { get; set; }
     }
-}
